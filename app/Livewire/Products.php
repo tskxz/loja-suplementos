04687afac31f137e38b5a63f882192278e7ddc1b
Tanjil <tanjilkh@gmail.com>
2024-07-20 19:43:34 +0100

    Update Products.php

--- app/Livewire/Products.php
+++ app/Livewire/Products.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Products extends Component
 {
+
+    use WithPagination;
     public function render()
     {
-        return view('livewire.products');
+        $products = Product::paginate(10);
+        return view('livewire.products', [
+            'products' => $products,
+        ]);
     }
 }
