a83dad12a40f5482368e1380cfb1ff4f6fcc0283
Tanjil <tanjilkh@gmail.com>
2024-07-20 19:46:55 +0100

    Update Products.php

--- app/Livewire/Products.php
+++ app/Livewire/Products.php
@@ -9,6 +9,14 @@ use Livewire\WithPagination;
 class Products extends Component
 {
 
+    public $product;
+    public $confirmingProductUpdate = false;
+
+    protected $rules = [
+        'product.name' => 'required|string|min:3',
+        'product.price' => 'required|numeric|beetween:1,100',
+    ];
+
     use WithPagination;
     public function render()
     {
@@ -17,4 +25,6 @@ class Products extends Component
             'products' => $products,
         ]);
     }
+
+
 }
