57464e81d9dcdea633d0f6f5cb5699ba74c23170
Tanjil <tanjilkh@gmail.com>
2024-07-20 20:47:06 +0100

    acrescentar rota

--- resources/views/livewire/produtos.blade.php
+++ resources/views/livewire/produtos.blade.php
@@ -1,3 +1,53 @@
-<div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+<x-slot name="header">
+    <h2 class="text-center">Produtos</h2>
+</x-slot>
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+            @if (session()->has('message'))
+            <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3"
+                role="alert">
+                <div class="flex">
+                    <div>
+                        <p class="text-sm">{{ session('message') }}</p>
+                    </div>
+                </div>
+            </div>
+            @endif
+            <button wire:click="create()"
+                class="my-4 inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base font-bold text-white shadow-sm hover:bg-red-700">
+                Create produto
+            </button>
+            @if($isModalOpen)
+            @include('livewire.create')
+            @endif
+            <table class="table-fixed w-full">
+                <thead>
+                    <tr class="bg-gray-100">
+                        <th class="px-4 py-2 w-20">No.</th>
+                        <th class="px-4 py-2">Nome</th>
+                        <th class="px-4 py-2">Preco</th>
+                        <th class="px-4 py-2">Quantidade</th>
+                        <th class="px-4 py-2">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($produtos as $produto)
+                    <tr>
+                        <td class="border px-4 py-2">{{ $produto->id }}</td>
+                        <td class="border px-4 py-2">{{ $produto->nome }}</td>
+                        <td class="border px-4 py-2">{{ $produto->preco}}</td>
+                        <td class="border px-4 py-2">{{ $produto->quantidade}}</td>
+                        <td class="border px-4 py-2">
+                            <button wire:click="edit({{ $produto->id }})"
+                                class="flex px-4 py-2 bg-gray-500 text-gray-900 cursor-pointer">Edit</button>
+                            <button wire:click="delete({{ $produto->id }})"
+                                class="flex px-4 py-2 bg-red-100 text-gray-900 cursor-pointer">Delete</button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Produtos;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('produtos', Produtos::class);
 
 Route::middleware([
     'auth:sanctum',
