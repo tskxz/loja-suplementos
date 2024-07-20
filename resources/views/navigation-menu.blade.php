ef525a291a08bfc0337127e5203e33d88287d862
Tanjil <tanjilkh@gmail.com>
2024-07-20 19:40:12 +0100

    navbar products

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,11 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <!-- Products Link -->
+                    <x-nav-link href="{{ route('products') }}" :active="request()->routeIs('products')">
+                        {{ __('Products') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -141,6 +146,11 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            <!-- Products Link -->
+            <x-responsive-nav-link href="{{ route('products') }}" :active="request()->routeIs('products')">
+                {{ __('Products') }}
             </x-responsive-nav-link>
         </div>
 
