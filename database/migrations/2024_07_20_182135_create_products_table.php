05def93d2c8ac9ac99fc57947657af430b5408d4
Tanjil <tanjilkh@gmail.com>
2024-07-20 19:24:11 +0100

    Update 2024_07_20_182135_create_products_table.php

--- database/migrations/2024_07_20_182135_create_products_table.php
+++ database/migrations/2024_07_20_182135_create_products_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->double("price", 8, 2);
             $table->timestamps();
         });
     }
