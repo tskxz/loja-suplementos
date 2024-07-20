afe7dfcaec66ebc6ddbb706607d288776d6eab53
Tanjil <tanjilkh@gmail.com>
2024-07-20 20:32:20 +0100

    adicionar campos

--- database/migrations/2024_07_20_192746_create_produtos_table.php
+++ database/migrations/2024_07_20_192746_create_produtos_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome', 100);
+            $table->double('preco', 8, 2);
+            $table->integer('quantidade');
             $table->timestamps();
         });
     }
