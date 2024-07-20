2ff31ebe1e5a27519519020781fe934082008eae
Tanjil <tanjilkh@gmail.com>
2024-07-20 21:49:24 +0100

    criar componentes categoria

---
+++ app/Livewire/Categorias.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Categorias extends Component
+{
+    public function render()
+    {
+        return view('livewire.categorias');
+    }
+}
