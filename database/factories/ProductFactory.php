1ce7e7c2cbc63ec677a2d53d7e33c5afdff70d39
Tanjil <tanjilkh@gmail.com>
2024-07-20 19:31:04 +0100

    create livewire component

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,7 +17,8 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->word(),
+            'price' => $this->faker->randomNumber(2),
         ];
     }
 }
