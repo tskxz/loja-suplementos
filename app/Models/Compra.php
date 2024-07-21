b77066953a3da38a6f8d2575bb23e408b89d1c08
Tanjil <tanjilkh@gmail.com>
2024-07-21 17:36:29 +0100

    Update Compra.php

--- app/Models/Compra.php
+++ app/Models/Compra.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Carrinho;
+use App\Models\User;
 
 class Compra extends Model
 {
     use HasFactory;
     protected $fillable = ['user_id', 'carrinho_id', 'status'];
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+
+    public function carrinho(): BelongsTo{
+        return $this->belongsTo(Carrinho::class);
+    }
 }
