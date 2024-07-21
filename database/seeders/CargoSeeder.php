c5cd21eff8e672a6760fef96e1cef4c84787df7a
Tanjil <tanjilkh@gmail.com>
2024-07-21 16:29:21 +0100

    Update CargoSeeder.php

--- database/seeders/CargoSeeder.php
+++ database/seeders/CargoSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CargoSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class CargoSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('cargos')->insert([
+            'nome' => 'admin',
+        ]);
+        DB::table('cargos')->insert([
+            'nome' => 'user',
+        ]);
     }
 }
