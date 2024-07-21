080e0be6047776410545d50103dd79b4caedbe57
Tanjil <tanjilkh@gmail.com>
2024-07-21 16:31:21 +0100

    Update Cargo.php

--- app/Models/Cargo.php
+++ app/Models/Cargo.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Cargo extends Model
 {
     use HasFactory;
     protected $fillable = ['nome'];
+
+    public function users(): HasMany{
+        return $this->hasMany(User::class);
+    }
 }
