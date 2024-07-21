6460202847492c8583043fac0762291ae8b4e792
Tanjil <tanjilkh@gmail.com>
2024-07-21 09:46:46 +0100

    Update Categoria.php

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Produto;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Categoria extends Model
 {
     protected $fillable = ['nome'];
     use HasFactory;
+    
+    public function produtos(): HasMany{
+        return $this->hasMany(Produto::class);
+    }
 }
