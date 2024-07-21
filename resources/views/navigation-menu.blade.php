adf9c9475797db15a1be73cfe2c01d0481e4c67f
Tanjil <tanjilkh@gmail.com>
2024-07-21 12:43:26 +0100

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -109,7 +109,9 @@
                             <div class="block px-4 py-2 text-xs text-gray-400">
                                 {{ __('Manage Account') }}
                             </div>
-
+                            <x-dropdown-link href="{{ route('carrinhos') }}">
+                                {{ __('Carrinho') }}
+                            </x-dropdown-link>
                             <x-dropdown-link href="{{ route('profile.show') }}">
                                 {{ __('Profile') }}
                             </x-dropdown-link>
